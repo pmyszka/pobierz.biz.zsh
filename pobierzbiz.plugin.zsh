ad86c18a42cae7b636a866aec0b26422507b0ea5
Piotr Myszka <piotr.myszka@gmail.com>
2017-05-08 14:36:09 +0100

    fixed a few bugs

--- pobierzbiz.plugin.zsh
+++ pobierzbiz.plugin.zsh
@@ -1,38 +1,38 @@
 pobierzbiz() {
-	local lock="/tmp/pobierzbiz.lock"
-	[[ ! -f $lock ]] || { echo "Lock file present: $lock. Remove it and run the command again."; return }
-	trap "rm -f $lock" EXIT
-
 	[[ -n $POBIERZBIZ_USER ]] || { echo "POBIERZBIZ_USER not defined."; return }
 	[[ -n $POBIERZBIZ_PASS ]] || { echo "POBIERZBIZ_PASS not defined."; return }
 
-	local url="http://pobierz.biz"
-	local curl=(curl -s -L -b $lock)
+	local lock="/tmp/pobierzbiz.lock"
+	[[ ! -f $lock ]] || { echo "Lock file present: $lock. Remove it and run the command again."; return }
+	trap "rm -f $lock" EXIT
+	touch $lock
 
-	$curl --dump-header $lock -d "$v=konto|main&f=loginUzt&c=aut&usr_login=$POBIERZBIZ_USER&usr_pass=$POBIERZBIZ_PASS" $url > /dev/null
-	local res=`$curl $url"/konto"`
-	grep -s -q -e "Zalogowany jako" <<< "$res" || { echo "Failed to log in."; return }
-	local uid=`grep -s -e "name='usr' value='[[:digit:]]\+'" <<< "$res" | tr -cd "[0-9]\n"`
+	local url="http://pobierz.biz"
+	res=$(curl -s -L b $lock --dump-header $lock -d "v=konto|main&f=loginUzt&c=aut&usr_login=$POBIERZBIZ_USER&usr_pass=$POBIERZBIZ_PASS" $url)
+	res=$(curl -s -L -b $lock $url/konto)
+	local uid=$(echo "$res" | grep -o -E "name='usr' value='[0-9]+'" | grep -o -E '[0-9]+')
+	[[ -n $uid ]] || { echo "Failed to log in."; return }
 
 	while (( $# > 0 )); do
 
-		res=`$curl -d "c=pob&v=usr,sprawdzone|usr,linki&f=sprawdzLinki&linki=$1%0A" $url`
-		if [[ ! -n `grep -o 'link_ok\[1\]' <<< "$res"` ]]; then
+		res=$(curl -s -L -b $lock -d "c=pob&v=usr,sprawdzone|usr,linki&f=sprawdzLinki&linki=$1%0A" $url)
+		if [[ -z $(echo "$res" | grep -o -E 'link_ok\[1\]') ]]; then
 			echo "Failed to download file $1"
 			shift
 			continue
 		fi
 
-		res=`$curl -d "c=pob&v=usr,pliki|usr,linki&f=zapiszRozpoczete&usr=$uid&progress_type=verified&link_ok[1]=$1" $url`
-		local fid=`grep -o -e "<input type='checkbox' name='fil\[[[:digit:]]\+\]' /></td><td>1</td>" <<< "$res" | grep -o -e "\[\([[:digit:]]\)\+]" | tr -cd "0-9\n"`
-		res=`$curl -d "c=fil&v=usr,pliki&fil[$fid]=on&perm=wygeneruj linki" $url`
-		res=`sed ':a;N;$!ba;s/\n/ /g' <<< "$res" | grep -o -e "<h2>Wygenerowane linki bezpośrednie</h2><textarea.*</textarea>" | grep -o -e "<textarea .*</textarea>" | sed -e 's,.*<textarea .*>\([^<]*\)</textarea>.*,\1,g'`
-		[[ -n $res ]] || { echo "Something went wrong"; return }
+		res=$(curl -s -L -b $lock -d "c=pob&v=usr,pliki|usr,linki&f=zapiszRozpoczete&usr=$uid&progress_type=verified&link_ok[1]=$1" $url)
+		local fid=$(echo "$res" | grep -o -E 'fil\[[0-9]+\]' | grep -o -E '[0-9]+')
+		res=$(curl -s -L -b $lock -d "c=fil&v=usr,pliki&fil[$fid]=on&perm=wygeneruj linki" $url)
+		# first replace newlines with spaces
+		res=$(echo "$res" | sed ':a;N;$!ba;s/\n/ /g' | grep -o -E "<textarea.*</textarea>" | sed -e 's/<textarea .*>\([^<]*\)<\/textarea>/\1/g')
+		[[ -n $res ]] || { echo "Something went wrong."; return }
 
 		echo "Downloading $1"
 		curl -L -# -O -C - $res
 
-		$curl -d "c=fil&v=usr,pliki&f=usunUsera&fil[$fid]=on" $url > /dev/null
+		res=$(curl -s -L -b $lock -d "c=fil&v=usr,pliki&f=usunUsera&fil[$fid]=on" $url)
 
 		shift
 	done
